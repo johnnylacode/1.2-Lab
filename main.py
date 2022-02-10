06dd1574677d0cd4786586261d73c0b6c30ca805
johnnylacode <98317518+johnnylacode@users.noreply.github.com>
2022-02-09 20:51:34 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+print('Enter integer:')
+user=int(input())
+print('You entered:', user)
+print(user, 'squared is', user ** 2)
+print('And', user, 'cubed is', user ** 3, '!!')
+print('Enter another integer:')
+user2=int(input())
+print(user, '+', user2, 'is', user+user2)
+print(user, '*', user2, 'is', user*user2)
+
+
+
+
